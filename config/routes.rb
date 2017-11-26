206613ff252ee2dc56a9749dd040c3e6c945f308
Betty Mutai <bettymutai00@gmail.com>
2017-11-26 18:06:42 +0300

    Added gem devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :movies
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/password',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#index'
-  resources :movies
 end
